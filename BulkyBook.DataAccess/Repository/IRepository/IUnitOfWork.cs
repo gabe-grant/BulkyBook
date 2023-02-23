65c1b2bc39450a135f756149a453ed0aa0bfde50
Gabe Grant <gabe.grant@proton.me>
2023-02-23 10:59:08 -0500

    new CoverType Model, Migration, and repository.

--- BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,6 +10,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
         void Save();
     }
 }
