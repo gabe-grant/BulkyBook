a572bca971211fc9b29fa31f66c4bfdf0863b172
Gabe Grant <gabe.grant@proton.me>
2023-02-28 19:38:08 -0500

    Added a product model and controller and a product view model

---
+++ BulkyBook.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ProductVM
+    {
+        // Tightly binded view binds your View to a ViewModel. Now when we add a property to display/update we can do it here
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; }
+    }
+}
---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿
+using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        // GET
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            // if it's null or zero the db will know to create a product
+            if (id == null || id == 0)
+            {
+                // ViewBag internally inserts data into the ViewData dictionary. So the key of ViewData and property of ViewBag must NOT match.
+                // ViewBag and ViewData are not ideal because in such cases are views are not tightly binded to one particular model. One View One Model
+                // If you wanted to make one model for two different views (like insert/update "upsert") you can create a ViewModel
+
+                // ViewBag.CategoryList = CategoryList;
+                // ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
+            }
+            else 
+            {
+                // update the product
+
+            }
+
+
+            return View(productVM);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var coverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+
+            if (coverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            return View(coverTypeFromDbFirst);
+        }
+
+        // POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
